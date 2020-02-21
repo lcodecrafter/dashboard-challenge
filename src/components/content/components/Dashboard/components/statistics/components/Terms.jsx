614d02523d1ef268dd227f7d9304225032ce9b9f
lcodecrafter <lcodecrafter@gmail.com>
2020-02-21 10:45:30 +0100

    Added terms component(finish). Added some generic media queries.

--- src/components/content/components/Dashboard/components/statistics/components/Terms.jsx
+++ src/components/content/components/Dashboard/components/statistics/components/Terms.jsx
@@ -1,8 +1,27 @@
 import React from "react";
 import "./terms.scss";
 
+//props:{terms:{[term:string] : value:string}}
 const Terms = props => {
-  return <div></div>;
+  const termsKeys = Object.keys(props.terms);
+  const termsValues = Object.values(props.terms);
+
+  return (
+    <div className="terms-container">
+      <div className="terms-keys">
+        {termsKeys.map((tk, index) => (
+          <div key={tk + index}>
+            {tk.slice(0, tk.length - 1)}&nbsp;{tk.slice(-1)}
+          </div>
+        ))}
+      </div>
+      <div className="terms-values">
+        {termsValues.map((tv, index) => (
+          <div key={tv + index}>{tv}</div>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default Terms;
--- src/components/content/components/Dashboard/components/statistics/index.jsx
+++ src/components/content/components/Dashboard/components/statistics/index.jsx
@@ -1,8 +1,11 @@
 import React from "react";
 import ChartCircle from "./components/ChartCircle";
+import Terms from "./components/Terms";
 import graphData from "../../fake-data/graphData.json";
+import terms from "../../fake-data/terms.json";
 import "./statistics.scss";
 
+//what comes from graphData should be passed as props as consequence of http request
 const Statistics = props => {
   return (
     <div className="stats-container">
@@ -20,7 +23,9 @@ const Statistics = props => {
           graph={graphData.ratings.graph}
         ></ChartCircle>
       </div>
-      <div className="boxType2"></div>
+      <div className="boxType2">
+        <Terms terms={terms}></Terms>
+      </div>
     </div>
   );
 };
